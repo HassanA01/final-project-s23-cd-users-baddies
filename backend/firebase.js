f2fec09140cbb7a5943ed2543f2c20defcfc1dc2
HassanA01 <anikeboss@gmail.com>
2023-06-15 00:12:39 -0400

    connected firebase db to backend, added GET request to read all documents

---
+++ backend/firebase.js
@@ -0,0 +1,16 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+var serviceAccount = require("../admin.json");
+
+initializeApp(
+    {
+        credential: cert(serviceAccount),
+        databaseURL: "https://cd-user-baddies.firebaseio.com",
+    }
+);
+const db = getFirestore();
+
+
+
+module.exports = { db }
