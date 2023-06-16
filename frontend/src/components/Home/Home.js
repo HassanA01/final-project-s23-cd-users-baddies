757df6b9a73ad3dd7e629a102405c04742b9d0e2
zaben316 <67436646+zaben316@users.noreply.github.com>
2023-06-16 08:24:23 -0400

    added home page template

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,3 +1,23 @@
+// import "./styles.css";
+
 export default function Home() {
-    return <h1>Home</h1>
+    return <>
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-nav-item"><a href="#page-top">Home</a></li>
+                <li class="sidebar-nav-item"><a href="#about">Message</a></li>
+                <li class="sidebar-nav-item"><a href="#services">Post</a></li>
+                <li class="sidebar-nav-item"><a href="#portfolio">Discover</a></li>
+                <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h1 class="mb-1">BizReach</h1>
+                <h3 class="mb-5"><em>A Free Way To Find Your Way Out</em></h3>
+                <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+            </div>
+        </header>
+    </>
 }
