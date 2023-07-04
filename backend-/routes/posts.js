c86d2000781686860bffbab763f3f06b11027bf2
anas khan <maste@Anass-MacBook-Pro.local>
2023-07-03 21:16:06 -0400

    split backend into routes and controllers

--- backend-/routes/posts.js
+++ backend-/routes/posts.js
@@ -1,87 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const { db } = require('../firebase');
+const postsController = require('../controllers/postsController');
 
-// Route to retrieve user data by UID
-router.get('/users/:uid', async (req, res) => {
-    const { uid } = req.params;
-  
-    try {
-      const docRef = db.collection('Users').doc(uid);
-      const docSnap = await docRef.get();
-  
-      if (docSnap.exists) {
-        const userData = docSnap.data();
-        res.status(200).json(userData);
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    } catch (error) {
-      console.error('Error retrieving user:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
-  
-  // Route to retrieve posts of a user by UID
-  router.get('/users/:uid/posts', async (req, res) => {
-    const { uid } = req.params;
-  
-    try {
-      const docRef = db.collection('Users').doc(uid);
-      const docSnap = await docRef.get();
-  
-      if (docSnap.exists) {
-        const userData = docSnap.data();
-        const posts = userData.posts || []; // Assuming posts field is an array in the user document
-        res.status(200).json(posts);
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    } catch (error) {
-      console.error('Error retrieving user posts:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+// Route to retrieve posts of a user by UID
+router.get('/users/:uid/posts', async (req, res) => {
+  const { uid } = req.params;
 
-  // Route to create a new post for a user
+  try {
+    const posts = await postsController.getUserPosts(uid);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Route to create a new post for a user
 router.post('/users/:uid/posts', async (req, res) => {
-    const { uid } = req.params;
-    const { title, description, price, location, postalCode } = req.body;
-  
-    try {
-      const docRef = db.collection('Users').doc(uid);
-      const docSnap = await docRef.get();
-  
-      if (docSnap.exists) {
-        const userData = docSnap.data();
-        const newPost = {
-          title,
-          description,
-          price,
-          location,
-          postalCode,
-          postedBy: uid,
-        };
-  
-        if (!userData.posts) {
-          userData.posts = [];
-        }
-        userData.posts.push(newPost);
-  
-        await docRef.update({ posts: userData.posts });
-  
-        res.status(200).json({ message: 'Post created successfully' });
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    } catch (error) {
-      console.error('Error creating post:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
-  
+  const { uid } = req.params;
+  const { title, description, price, location, postalCode } = req.body;
 
+  try {
+    const newPost = {
+      title,
+      description,
+      price,
+      location,
+      postalCode,
+      postedBy: uid,
+    };
 
+    const result = await postsController.createPost(uid, newPost);
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Define more routes as needed
 
