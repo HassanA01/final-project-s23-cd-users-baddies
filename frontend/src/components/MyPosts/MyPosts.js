78fdd8eff6d48a90e5bb4763294b62cd3a335723
HassanA01 <anikeboss@gmail.com>
2023-07-18 18:11:48 -0400

    created new card to add status functionality

--- frontend/src/components/MyPosts/MyPosts.js
+++ frontend/src/components/MyPosts/MyPosts.js
@@ -1,11 +1,41 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import MyVerticallyCenteredModal from './PopUp/PopUp';
 import { UserContext } from '../User/UserContext';
-import { Button, Box, Divider, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter } from "@chakra-ui/react";
-import { getFirestore, doc, getDoc, updateDoc, arrayRemove, collection, arrayUnion, query, where, getDocs } from "firebase/firestore";
+import {
+  Card,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  ButtonGroup,
+  CardBody,
+  CardFooter,
+  Button,
+  Box,
+  Divider,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from "@chakra-ui/react";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 const db = getFirestore(); // assume firebase is already initialized
 
 const MyPosts = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
   const user = useContext(UserContext);
   const [posts, setPosts] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
@@ -39,30 +69,44 @@ const MyPosts = () => {
       setPosts(updatedPosts);
 
       // Find all businesses who have this post in their gigs
-      const businessesSnapshot = await getDocs(query(collection(db, 'Users'), where('userType', '==', 'business')));
+      const businessesSnapshot = await getDocs(
+        query(collection(db, 'Users'), where('userType', '==', 'business'))
+      );
       businessesSnapshot.forEach(async (businessDoc) => {
         const businessData = businessDoc.data();
 
-        if (businessData.Gigs && businessData.Gigs.find((gig) => gig.postId === updatedPost.id)) {
+        if (
+          businessData.Gigs &&
+          businessData.Gigs.find((gig) => gig.postId === updatedPost.id)
+        ) {
           // Map over gigs, update the one with matching postId
           const updatedGigs = businessData.Gigs.map((gig) =>
-            gig.postId === updatedPost.id ? { ...gig, status: 'completed' } : gig
+            gig.postId === updatedPost.id
+              ? { ...gig, status: 'completed' }
+              : gig
           );
 
           // Update gigs in Firestore
-            await updateDoc(doc(db, 'Users', businessData.uid), { Gigs: updatedGigs });
-            const newMessage = {
-                text: `Job '${currentPost.title}' has been completed.`,
-                timestamp: Date.now(),
-                type: 'notification',
-              };
-            
-              // Add the new message to business's messages array
-              // Check if the messages array exists. If not, initialize it.
-              let updatedMessages = businessData.messages ? [...businessData.messages, newMessage] : [newMessage];
-            
-              // Update gigs and messages in Firestore
-              await updateDoc(doc(db, 'Users', businessData.uid), { Gigs: updatedGigs, messages: updatedMessages });
+          await updateDoc(doc(db, 'Users', businessData.uid), {
+            Gigs: updatedGigs,
+          });
+          const newMessage = {
+            text: `Job '${currentPost.title}' has been completed.`,
+            timestamp: Date.now(),
+            type: 'notification',
+          };
+
+          // Add the new message to business's messages array
+          // Check if the messages array exists. If not, initialize it.
+          let updatedMessages = businessData.messages
+            ? [...businessData.messages, newMessage]
+            : [newMessage];
+
+          // Update gigs and messages in Firestore
+          await updateDoc(doc(db, 'Users', businessData.uid), {
+            Gigs: updatedGigs,
+            messages: updatedMessages,
+          });
         }
       });
     }
@@ -78,29 +122,66 @@ const MyPosts = () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, []); // Empty dependency array to run the effect only once
+
+  const onViewMap = (post) => {
+    setSelectedPost(post);
+    setModalShow(true);
+  };
 
   return (
-    <Box maxW="sm" borderWidth={1} borderRadius="lg" overflow="hidden" mt={6}>
-      <Box p="6">
-        {posts.map((post, index) => (
-          <Box key={index} d="flex" alignItems="baseline" mb={4}>
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-            >
-              {post.title}
+    <div>
+      <h1 className="page-title" style={{ marginBottom: "50px", position: "sticky", top: "0" }}>
+        My Posts
+      </h1>
+      <div>
+        {posts.length > 0 && (
+          <div className="post-container">
+            <Box display="flex" flexWrap="wrap">
+              {posts.map((post) => (
+                <Card key={post.id} maxW="sm" flex="1" mr={10} mb={4}>
+                  <Box borderWidth={1} borderRadius="lg" overflow="hidden">
+                    <CardBody>
+                      {/* <Image
+                        src={post.image}
+                        alt={post.title}
+                        borderRadius="lg"
+                      /> */}
+                      <Stack mt='6' spacing='3'>
+                        <Heading size='lg'>{post.title}</Heading>
+                        <Text fontSize='md'>{post.description}</Text>
+                        <Text color='blue.600' fontSize='2xl'>
+                          ${post.price}
+                        </Text>
+                      </Stack>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter>
+                      <ButtonGroup spacing='2'>
+                        <Button variant='solid' colorScheme='blue'>
+                          Details
+                        </Button>
+                        <Button variant='solid' colorScheme='teal'>
+                          Complete
+                        </Button>
+                      </ButtonGroup>
+                    </CardFooter>
+                  </Box>
+                </Card>
+              ))}
             </Box>
-            <Divider mt={2} mb={2} />
-            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-              Status: {post.status}
-            </Box>
-            <Button id={post.postId} colorScheme="teal" size="xs" mt={3} onClick={() => promptCompleteDialog(post)}>Complete Job</Button>
-          </Box>
-        ))}
+          </div>
+        )}
+
+        {/* Render the modal when modalShow is true */}
+        {modalShow && (
+          <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            post={selectedPost} // Pass the selected post as a prop to the modal
+          />
+        )}
+
         <AlertDialog
           isOpen={isOpen}
           leastDestructiveRef={btnRef}
@@ -127,8 +208,8 @@ const MyPosts = () => {
             </AlertDialogContent>
           </AlertDialogOverlay>
         </AlertDialog>
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 };
 
