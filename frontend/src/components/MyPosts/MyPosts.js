43f1314bf23dd7b41ddfdd1229d6536bdc9f9648
HassanA01 <anikeboss@gmail.com>
2023-07-19 21:49:41 -0400

    created modal for cards of all statuses

--- frontend/src/components/MyPosts/MyPosts.js
+++ frontend/src/components/MyPosts/MyPosts.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../User/UserContext';
+import getAddressFromCoordinates from '../../utils/utils';
 import {
   Card,
   Stack,
@@ -11,6 +12,14 @@ import {
   Button,
   Box,
   Divider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure 
 } from "@chakra-ui/react";
 import axios from 'axios';
 
@@ -18,7 +27,7 @@ import axios from 'axios';
 const MyPosts = () => {
   const user = useContext(UserContext);
   const [posts, setPosts] = useState([]);
-  const [filteredStatus, setFilteredStatus] = useState("open");
+  const [filteredStatus, setFilteredStatus] = useState("posted");
 
   const filterPostsByStatus = (status) => {
     setFilteredStatus(status);
@@ -27,9 +36,15 @@ const MyPosts = () => {
   useEffect(() => {
     const fetchUserPosts = async () => {
       try {
-        console.log(user.uid)
+        // console.log(user.uid);
         const response = await axios.get(`http://localhost:3000/api/posts/users/${user.uid}/posts`);
-        setPosts(response.data);
+        const postsWithAddresses = await Promise.all(
+          response.data.map(async (post) => {
+            const address = await getAddressFromCoordinates(post.location.lat, post.location.lon);
+            return { ...post, address };
+          })
+        );
+        setPosts(postsWithAddresses);
       } catch (error) {
         console.error('Error fetching user posts:', error);
       }
@@ -38,7 +53,8 @@ const MyPosts = () => {
     fetchUserPosts();
   }, [user.uid]);
   
-
+   
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <div>
@@ -73,10 +89,10 @@ const MyPosts = () => {
             {posts
               .filter((post) => post.status === filteredStatus)
               .map((post) => (
-                <Card key={post.id} maxW="sm" flex="1" mr={10} mb={4}>
+                <Card key={post.id} maxW="sm" flex="none" width="250px" m={6}>
                   <Box borderWidth={1} borderRadius="lg" overflow="hidden">
                     <CardBody>
-                      <Stack mt="6" spacing="3">
+                      <Stack mt="6" spacing="4">
                         <Heading size="lg">{post.title}</Heading>
                         <Text fontSize="lg">Status: {post.status}</Text>
                         <Text color="blue.600" fontSize="xl">
@@ -86,13 +102,39 @@ const MyPosts = () => {
                     </CardBody>
                     <Divider />
                     <CardFooter>
-                      <ButtonGroup spacing="2">
-                        <Button variant="solid" colorScheme="blue">
+                      <ButtonGroup spacing="2" margin="10px" >
+                        <Button onClick={onOpen} variant="solid" colorScheme="blue" ml={post.status === "posted" || post.status === "completed"? "50px" : "flex-start"} >
                           Details
                         </Button>
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                          <ModalOverlay />
+                          <ModalContent
+                            position="absolute"
+                            top="25%"
+                            transform="translate(-50%, -50%)"
+                            maxW="80%" // Optional: Set the maximum width of the modal
+                            width="fit-content" // Optional: Adjust the width based on the modal content
+                          >
+                            <ModalHeader>{post.title}</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                              <Text>Description: {post.description}</Text>
+                              <Text>Location: {post.address}</Text>
+                              <Text>Price: ${post.price}</Text>
+                            </ModalBody>
+
+                            <ModalFooter>
+                              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Close
+                              </Button>
+                            </ModalFooter>
+                          </ModalContent>
+                        </Modal>
+                        {post.status === "in-progress" && (
                         <Button variant="solid" colorScheme="teal">
                           Complete
                         </Button>
+                      )}
                       </ButtonGroup>
                     </CardFooter>
                   </Box>
