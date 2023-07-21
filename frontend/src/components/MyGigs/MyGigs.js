9b2ac6147ac7f9608261572741e4eb37d7f17d22
zaben316 <67436646+zaben316@users.noreply.github.com>
2023-07-21 14:40:38 -0400

    added discover gigs page

--- frontend/src/components/MyGigs/MyGigs.js
+++ frontend/src/components/MyGigs/MyGigs.js
@@ -1,50 +1,151 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../User/UserContext';
-import { Button, Box, Divider } from "@chakra-ui/react";
-import { getFirestore, doc, getDoc, updateDoc, arrayRemove} from "firebase/firestore";
+import getAddressFromCoordinates from '../../utils/utils';
+import {
+  Card,
+  Stack,
+  Heading,
+  Text,
+  ButtonGroup,
+  CardBody,
+  CardFooter,
+  Button,
+  Box,
+  Divider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure 
+} from "@chakra-ui/react";
+import axios from 'axios';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-const db = getFirestore(); // assume firebase is already initialized
+const db = getFirestore(); // Assume Firebase is already initialized
 
 const MyGigs = () => {
   const user = useContext(UserContext);
-  const [Gigs, setGigs] = useState([]);
+  const [gigs, setGigs] = useState([]);
+  const [filteredStatus, setFilteredStatus] = useState("request-gig");
 
-  const fetchGigs = async () => {
-    const userRef = doc(db, 'Users', user.uid);
-    const userSnapshot = await getDoc(userRef);
+  const filterPostsByStatus = (status) => {
+    setFilteredStatus(status);
+  };
 
-    if (userSnapshot.exists() && userSnapshot.data().Gigs) {
-      setGigs(userSnapshot.data().Gigs);
-    }
-  }
 
   useEffect(() => {
-    fetchGigs();
-  }, []);
+    const fetchUserGigs = async () => {
+      try {
 
+        const response = await axios.get(`http://localhost:3000/api/gigs/users/${user.uid}/gigs`);
+        const gigsData = await Promise.all(
+          response.data.map(async (gig) => {
+            console.log(gig);
+            return { ...gig };
+          })
+        );
+        setGigs(gigsData);
+      } catch (error) {
+        console.error('Error fetching user posts:', error);
+      }
+    };
+    fetchUserGigs()
+  }, [user.uid]);
+
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <Box maxW="sm" borderWidth={1} borderRadius="lg" overflow="hidden" mt={6}>
-      <Box p="6">
-        {Gigs.map((gig, index) => (
-          <Box key={index} d="flex" alignItems="baseline" mb={4}>
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-            >
-              {gig.title}
-            </Box>
-            <Divider mt={2} mb={2} />
-            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-              Status: {gig.status}
-            </Box>
-            <Divider my={2} />
-          </Box>
-        ))}
+    <div>
+      <Box
+        position="fixed"
+        top={120}
+        left={0}
+        right={0}
+        display="flex"
+        justifyContent="center"
+        p={4}
+      >
+        <Stack spacing={3} direction="row" align="center" display="flex">
+          <Button colorScheme="teal" onClick={() => filterPostsByStatus("request-gig")}>
+            Posted
+          </Button>
+          <Button colorScheme="red" onClick={() => filterPostsByStatus("requested")}>
+            In Progress
+          </Button>
+          <Button colorScheme="teal" onClick={() => filterPostsByStatus("completed")}>
+            Completed
+          </Button>
+        </Stack>
       </Box>
-    </Box>
+      <Text mt={10} position="fixed" top={180} left={0} right={0} textAlign="center" fontSize="xl" fontWeight="bold">
+        {filteredStatus.charAt(0).toUpperCase() + filteredStatus.slice(1)}
+      </Text>
+
+      {gigs.filter((gig) => gig.status === filteredStatus).length > 0 && (
+        <div className="post-container">
+          <Box display="flex" flexWrap="wrap">
+            {gigs
+              .filter((gig) => gig.status === filteredStatus)
+              .map((gig) => (
+                <Card key={gig.id} maxW="sm" flex="none" width="250px" m={6}>
+                  <Box borderWidth={1} borderRadius="lg" overflow="hidden">
+                    <CardBody>
+                      <Stack mt="6" spacing="4">
+                        <Heading size="lg">GID: {gig.gid}</Heading>
+                        <Text fontSize="lg">Status: {gig.status}</Text>
+                        <Text color="blue.600" fontSize="xl">
+                          Price: ${gig.status}
+                        </Text>
+                      </Stack>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter>
+                      <ButtonGroup spacing="2" margin="10px" >
+                        <Button onClick={onOpen} variant="solid" colorScheme="blue" ml={gig.status === "posted" || gig.status === "completed" ? "50px" : "flex-start"} >
+                          Details
+                        </Button>
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                          <ModalOverlay />
+                          <ModalContent
+                            position="absolute"
+                            top="25%"
+                            transform="translate(-50%, -50%)"
+                            maxW="80%" // Optional: Set the maximum width of the modal
+                            width="fit-content" // Optional: Adjust the width based on the modal content
+                          >
+                            <ModalHeader>{gig.title}</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                              <Text>Description: {gig.description}</Text>
+                              <Text>Location: {gig.address}</Text>
+                              <Text>Price: ${gig.price}</Text>
+                            </ModalBody>
+
+                            <ModalFooter>
+                              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Close
+                              </Button>
+                            </ModalFooter>
+                          </ModalContent>
+                        </Modal>
+                        {gig.status === "in-progress" && (
+                          <Button variant="solid" colorScheme="teal">
+                            Complete
+                          </Button>
+                        )}
+                      </ButtonGroup>
+                    </CardFooter>
+                  </Box>
+                </Card>
+              ))}
+          </Box>
+        </div>
+      )}
+
+    </div>
   );
 };
 
