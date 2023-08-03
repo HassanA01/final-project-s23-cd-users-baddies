36912994ca48f8341f0f13d85f4bb7c12ac09252
Aneeq <aneeq.hassan@utoronto.ca>
2023-08-03 03:47:45 -0400

    added date to reviews card

--- frontend/src/components/MyBusiness/Reviews/ReviewCard.js
+++ frontend/src/components/MyBusiness/Reviews/ReviewCard.js
@@ -1,15 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { Card, CardBody, Heading, Text, Stack } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Text, Stack, Flex } from "@chakra-ui/react";
 import RatingStars from "./Rating";
 import { UserContext } from "../../User/UserContext";
 
 function ReviewCard({ review }) {
     const [customer, setCustomer] = useState('');
-    const rating = review.rating || 0;
-    const feedback = review.feedback || 'No feedback provided';
+    const rating = review.rating || 0; // Default to 0 if rating is not provided
+    const feedback = review.feedback || 'No feedback provided'; // Default message if feedback is not provided
+    const date = new Date(review.createdAt._seconds * 1000).toLocaleDateString()
     const user = useContext(UserContext);
-
     useEffect(() => {
       const fetchCustomerName = async () => {
         try {
@@ -34,7 +34,10 @@ function ReviewCard({ review }) {
           <CardBody>
             <Heading size='md' align="left">
               {customer}
-              <RatingStars rating={rating} />
+              <Flex>
+                <RatingStars rating={rating} />
+                <Text mt="9px" ml="10px" color="gray.600"> {date} </Text>
+              </Flex>
             </Heading>
 
             <Text py='2'>
